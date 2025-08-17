2827467bb782e89344af68adf5d585123398ed51
priyaram2005 <pram17547@gmail.com>
2025-08-17 19:22:08 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,16 @@
+def findDuplicate(arr):
+    slow = arr[0]
+    fast = arr[0]
+    while True:
+        slow = arr[slow]
+        fast = arr[arr[fast]]
+        if slow == fast:
+            break
+    slow = arr[0]
+    while slow != fast:
+        slow = arr[slow]
+        fast = arr[fast]
+    return slow
+
+arr = list(map(int, input('Enter an element of array:- ').split()))
+print("Duplicate number:", findDuplicate(arr))
