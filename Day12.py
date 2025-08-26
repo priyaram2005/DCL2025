4fa3f3f3e230836df5d3fb54c56a0f0a5b70499f
priyaram2005 <pram17547@gmail.com>
2025-08-26 20:38:15 +0530

    Create Day12.py

---
+++ Day12.py
@@ -0,0 +1,17 @@
+def isValid(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+    
+    for char in s:
+        if char in mapping:
+            top = stack.pop() if stack else '#'
+            if mapping[char] != top:
+                return False
+        else:
+            stack.append(char)
+    
+    return not stack
+
+
+s = input("Enter a string of parentheses: ").strip()
+print(isValid(s))
