1ea964d0c4d7277d4fcf5e29187673ff3f852536
priyaram2005 <pram17547@gmail.com>
2025-08-29 19:09:02 +0530

    Create Day15.py

---
+++ Day15.py
@@ -0,0 +1,16 @@
+def longest_unique_substring(s):
+    char_index = {}
+    left = 0
+    max_length = 0
+
+    for right in range(len(s)):
+        if s[right] in char_index and char_index[s[right]] >= left:
+            left = char_index[s[right]] + 1
+        char_index[s[right]] = right
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
+
+
+S = input("Enter a string: ")
+print("Output:", longest_unique_substring(S))
